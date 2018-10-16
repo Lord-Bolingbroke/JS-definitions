ac39504d174256ef824abd9af156559926c56f79
Noah Schooler and Tavish OKeefe <tavish@>
2018-10-16 14:05:37 -0700

    add seven definitions.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $(".c1").click(function() {
+    $("#definition-showing").toggle();
+    $("#definition-hidden").toggle();
+  });
+
+  $(".c2").click(function() {
+    $("#definition-showing1").toggle();
+    $("#definition-hidden1").toggle();
+  });
+
+  $(".c3").click(function() {
+    $("#definition-showing2").toggle();
+    $("#definition-hidden2").toggle();
+  });
+
+  $(".c4").click(function() {
+    $("#definition-showing3").toggle();
+    $("#definition-hidden3").toggle();
+  });
+
+  $(".c5").click(function() {
+    $("#definition-showing4").toggle();
+    $("#definition-hidden4").toggle();
+  });
+
+  $(".c6").click(function() {
+    $("#definition-showing5").toggle();
+    $("#definition-hidden5").toggle();
+  });
+
+  $(".c7").click(function() {
+    $("#definition-showing6").toggle();
+    $("#definition-hidden6").toggle();
+  });
+});
